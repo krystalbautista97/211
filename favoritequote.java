e28f5d17ed0d6ffe328c34c6381eb3b1a1204ed3
krystalbautista97 <115441008+krystalbautista97@users.noreply.github.com>
2023-01-22 17:29:49 -0800

    Add files via upload

---
+++ favoritequote.java
@@ -0,0 +1,7 @@
+public class favoritequote {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "My ideal and reality, they are too far apart but I still want to cross the bridge and reach the real me - RM";
+        System.out.println(output);
+    }
+}
